e1d7cc1ef927bcee63d30bccba3ba2e1857764f2
otunba32 <emmanuel.abia32@gmail.com>
2026-05-01 15:51:27 -0400

    Save progress: 960.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -3,7 +3,7 @@
 #Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-NUMBER=$(( RANDOM % 15 + 1 ))
+NUMBER=$(( RANDOM % 15+16 ))
 
 
 TEXT="The next number is,"
@@ -14,4 +14,23 @@ echo "Number Check"
 if (( NUMBER <= 15 ))
 then
   echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
+fi
+#!/bin/bash
+
+# Bingo Number Generator
+echo -e "\n~~ Bingo Number Generator ~~\n"
+
+NUMBER=$(( RANDOM % 15+16 ))
+
+TEXT="The next number is,"
+
+if (( NUMBER <= 15 ))
+then
+  echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+  echo $TEXT I:$NUMBER
 fi
