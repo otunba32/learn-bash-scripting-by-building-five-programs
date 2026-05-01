b3033328f3ade1a68546a9e62e9c5ae834867bf1
otunba32 <emmanuel.abia32@gmail.com>
2026-05-01 15:15:52 -0400

    Save progress: 817.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-#Program that counts down to zero from a given argument
-echo -e "\n~~ Countdown Timer ~~\n"
-if [[ $1 -gt 0 ]]
-then
-  : 'for (( i = $1; i >= 0; i-- ))
-do
-  echo $i 
-done
-sleep 1'
-I=$1
-fi
-echo $I
+#Bingo Number Generator
+echo -e "\n~~ Bingo Number Generator ~~\n"
+
+NUMBER=5
+echo $NUMBER
